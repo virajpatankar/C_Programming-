4d34d9ce2c8330c4db0bddf0ad2fa0d20d1c5c97
virajpatankar <virajpatankar70@gmail.com>
2025-08-24 13:19:15 -0400

    Assignment No 7

---
+++ Assignment No 7/02.Program to Create Array With Initializing & Display All Elements.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+
+        int Bill[5]={3,5,1,9,5,};
+        printf("\n=====================******=======================\n");
+
+        printf("\nArray 1st element is %d",Bill[0]);
+        printf("\nArray 2nd element is %d",Bill[1]);
+        printf("\nArray 3rd element is %d",Bill[2]);
+        printf("\nArray 4th element is %d",Bill[3]);
+        printf("\nArray 5th element is %d",Bill[4]);
+
+        printf("\n=====================******=========================\n");
+
+        getch();
+        return 0;
+
+}
