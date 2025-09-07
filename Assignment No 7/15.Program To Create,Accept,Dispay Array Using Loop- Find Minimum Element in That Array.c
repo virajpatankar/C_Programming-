b2d2e97e647617d526aed2dea0e4fd8d090bdc92
virajpatankar <virajpatankar70@gmail.com>
2025-09-07 02:35:54 -0400

    Assignment No 7

---
+++ Assignment No 7/15.Program To Create,Accept,Dispay Array Using Loop- Find Minimum Element in That Array.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+
+         int Arr[7]={}, i = 0, Min = 0;
+
+
+         for(i = 0; i < 7; i++)
+         {
+
+
+              printf("Enter Value of %d Element:",i+1);
+              scanf("%d",&Arr[i]);
+
+          }
+
+          getch();
+          printf("\n\n");
+
+
+          for(i = 0; i < 7; i++)
+          {
+
+
+               if(i==0 || Arr[i]<Min)
+               {
+
+                   Min=Arr[i];
+
+               }
+
+           }
+
+
+           getch();
+
+
+           printf("\n\nMaximum Array Element is :%d",Min);
+
+           getch();
+           return 0;
+
+}
