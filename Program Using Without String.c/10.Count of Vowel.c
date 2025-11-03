81ba8f46b940644c1f6a541315f12605378a6f7e
Viraj Patankar <virajpatankar70@gmail.com>
2025-11-03 00:18:07 -0400

    Program Using without String

---
+++ Program Using Without String.c/10.Count of Vowel.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cSrc[20] = {'\0'};
+    int i = 0, cnt = 0;
+
+    printf("\nEnter a string Get Count of Vowels :");
+    gets(cSrc);
+
+    while(cSrc[i] != '\0')
+    {
+        if((cSrc[i] == 'A') || (cSrc[i] == 'a') || (cSrc[i] == 'E') || (cSrc[i] == 'e') || (cSrc[i] == 'I') || (cSrc[i] == 'i') || (cSrc[i] == 'O') || (cSrc[i] == 'o') || (cSrc[i] == 'U') || (cSrc[i] == 'u'))
+        {
+            cnt++;
+        }
+        i++;
+    }
+
+    printf("\nCount of Vowels  is : %d",cnt);
+
+    getch();
+    return 0;
+
+}
